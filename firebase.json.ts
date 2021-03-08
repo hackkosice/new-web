f4577d15cdc3b3fa52a6561e0b4035a7d8986b25
Juraj Mičko <micko.juraj64@gmail.com>
2021-03-08 03:05:47 +0000

    Generate firebase redirects with lectures feedback links

---
+++ firebase.json.ts
@@ -0,0 +1,23 @@
+import { readFileSync, writeFileSync } from "fs"
+const fetch = require("node-fetch");
+
+async function generateFirebaseJson() {
+    const firebase = JSON.parse(readFileSync('firebase.base.json', 'utf8'))
+
+    const response = await fetch('http://bot.hackslovakia.com/lectures/feedback/get-redirects', {
+        headers: {
+            'Authorization': 'Basic ' + Buffer.from(process.env.PUFFS_USER + ":" + process.env.PUFFS_PASS).toString('base64')
+        }
+    })
+    const redirects = await response.json()
+
+    firebase.hosting.map(hosting => {
+        hosting.redirects = [...hosting.redirects, ...redirects]
+    })
+
+    writeFileSync('firebase.json', JSON.stringify(firebase, null, 2))
+
+    console.log("Generated firebase.json")
+}
+
+generateFirebaseJson()
