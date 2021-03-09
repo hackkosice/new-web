5ea3ef6f0a0e75c4762766a4c03eb057051dfa02
Juraj Mičko <micko.juraj64@gmail.com>
2021-03-09 04:16:41 +0000

    Call fill-slugs on each build

--- firebase.json.ts
+++ firebase.json.ts
@@ -4,11 +4,11 @@ const fetch = require("node-fetch");
 async function generateFirebaseJson() {
     const firebase = JSON.parse(readFileSync('firebase.base.json', 'utf8'))
 
-    const response = await fetch('http://bot.hackslovakia.com/lectures/feedback/get-redirects', {
-        headers: {
-            'Authorization': 'Basic ' + Buffer.from(process.env.PUFFS_USER + ":" + process.env.PUFFS_PASS).toString('base64')
-        }
-    })
+    const headers = {
+        'Authorization': 'Basic ' + Buffer.from(process.env.PUFFS_USER + ":" + process.env.PUFFS_PASS).toString('base64')
+    }
+    await fetch('https://bot.hackslovakia.com/lectures/feedback/fill-slugs', { headers })
+    const response = await fetch('https://bot.hackslovakia.com/lectures/feedback/get-redirects', { headers })
     const redirects = await response.json()
 
     firebase.hosting.map(hosting => {
